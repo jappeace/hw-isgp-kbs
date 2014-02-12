ca8274f4817cfe4311b11b66976028f3363e4bfa
Jappie Klooster <jappieklooster@hotmail.com>
2014-02-12 17:11:46 +0100

    added more explenaintion to IGridtraveller

--- ISGPKBS/Game/IGridTraveller.h
+++ ISGPKBS/Game/IGridTraveller.h
@@ -13,7 +13,10 @@
 namespace isgp{
 /*
  * 'pure virtual' or an interface, classes that extend this class will be able to traverse the
- * grid
+ * grid.
+
+ * a class implementing this interface can be given to the traverse* methods of grid and receive
+ * the tiles in the row column or grid.
  */
 	class IGridTraveller{
 
