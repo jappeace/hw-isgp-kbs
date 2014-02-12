70073c360d51b41a2158cd39b45ebbd860ff5d4a
Johnnei <johnnei07@live.nl>
2014-02-12 11:26:33 +0100

    Corrected comment style on Graphics.h

--- ISGPKBS/Game/Graphics.h
+++ ISGPKBS/Game/Graphics.h
@@ -9,23 +9,15 @@ namespace isgp {
 	class Graphics
 	{
 	private:
-		/**
-		 * The device context which is visible on the screen
-		 */
+		// The device context which is visible on the screen
 		HDC _hdc;
-		/**
-		 * The hidden device context on which we render to prevent flickering
-		 */
+		// The hidden device context on which we render to prevent flickering
 		HDC _backBuffer;
-		/**
-		 * The bitmap on which we are actually rendering in the background
-		 */
+		// The bitmap on which we are actually rendering in the background
 		HBITMAP _bitmap;
 
 #ifdef _DEBUG
-		/**
-		* FPScounter.
-		*/
+		// FPScounter.
 		TimesPerSecondCounter _fpsCounter;
 #endif
 
@@ -33,13 +25,9 @@ namespace isgp {
 		Graphics(HWND);
 		~Graphics(void);
 
-		/**
-		 * Prepares the device context to start rendering
-		 */
+		// Prepares the device context to start rendering
 		void BeginRendering(HWND, PAINTSTRUCT *);
-		/**
-		 * Ends the rendering state and draws the backbuffer onto the screen
-		 */
+		// Ends the rendering state and draws the backbuffer onto the screen
 		void EndRendering(HWND, PAINTSTRUCT *);
 		void drawStr(Point& position, string str);
 		void drawStr(Point& position, const char* str, int length);
