15c722aed48f16e6bb887a121246391d598405f2
Johnnei <johnnei07@live.nl>
2014-02-19 11:19:28 +0100

    Moved the behaviourinterface to the isgp namespace.
    Added class comment to it.

--- ISGPKBS/Game/behaviourinterface.h
+++ ISGPKBS/Game/behaviourinterface.h
@@ -1,10 +1,14 @@
 #pragma once
-class BehaviourInterface
-{
-public:
-	// Executes the implemented behaviour.
-	// The executing will keep in mind how many milliseconds have passed.
-	// This method must be overridden in a child class
-	virtual void Update(long ms) = 0;
-};
 
+namespace isgp {
+	// The default interface which allows an object to have certain behaviours.
+	// This interface does not define a behaviour by itself.
+	class BehaviourInterface
+	{
+	public:
+		// Executes the implemented behaviour.
+		// The executing will keep in mind how many milliseconds have passed.
+		// This method must be overridden in a child class
+		virtual void Update(const long ms) = 0;
+	};
+}
