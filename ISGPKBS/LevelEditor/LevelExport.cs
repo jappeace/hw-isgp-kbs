439d1c202b6c95c87643cda49beae4b589b04428
Joep Kemperman <kazooiej@gmail.com>
2014-02-19 15:36:31 +0100

    Implemented the levelexport.

--- ISGPKBS/LevelEditor/LevelExport.cs
+++ ISGPKBS/LevelEditor/LevelExport.cs
@@ -6,17 +6,36 @@ namespace LevelEditor
 {
 	class LevelExport : ILevelExporter
 	{
+		/// <summary>
+		/// Exports the specified level to the specified file. If the file
+		/// already exists, it will be deleted and a new file will be created.
+		/// </summary>
 		public void ExportLevel(ILevel level, string file)
 		{
 			IDictionary<Point, TileType> tiles = level.GetTiles();
-			var streamWriter = new StreamWriter(file);
-			foreach (Point key in tiles.Keys)
+
+			// Delete the level file if it already exists.
+			if (File.Exists(file))
 			{
+				File.Delete(file);
 			}
 
-			throw new System.NotImplementedException();
+			using (var streamWriter = new StreamWriter(file))
+			{
+
+				// Write width and height to the file.
+				streamWriter.WriteLine(string.Format("width={0}", level.Width));
+				streamWriter.WriteLine(string.Format("width={1}", level.Height));
+				foreach (Point key in tiles.Keys)
+				{
+					streamWriter.WriteLine(TileToString(key, tiles[key]));
+				}
+			}
 		}
 
+		/// <summary>
+		/// Creates a line for a tile.
+		/// </summary>
 		private string TileToString(Point position, TileType type)
 		{
 			return string.Format("{0},{1}={2}", position.X, position.Y, (int)type);
