25dc1635b8f2c70672a756e9ce3dabe406fa0334
Joep Kemperman <kazooiej@gmail.com>
2014-02-19 12:00:21 +0100

    Added TileType enumeration to the leveleditor project.

---
+++ ISGPKBS/LevelEditor/TileType.cs
@@ -0,0 +1,13 @@
+﻿namespace LevelEditor
+{
+	/// <summary>
+	/// Enumeration of tile types with their integer representation.
+	/// </summary>
+	enum TileType
+	{
+		Start = 0,
+		Finish = 1,
+		Brick = 2,
+		Grass = 3
+	}
+}
