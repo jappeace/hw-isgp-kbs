7028a82bf72f3d1cb70b05dac8c36102ff27c956
Jappie Klooster <jappieklooster@hotmail.com>
2014-02-19 12:44:13 +0100

    code now compiles

--- ISGPKBS/Game/Window.cpp
+++ ISGPKBS/Game/Window.cpp
@@ -36,25 +36,6 @@ void Window::OnPaint(Graphics* g){
 	_level->Paint(g);
 }
 
-void Window::OnCommand(int from, int command){
-	switch(from){
-	case ID_FILE_NEWGAME:
-		this->repaint();
-		break;
-	case ID_HELP_ABOUT:
-		MessageBox(
-			_hWnd,
-			// message:
-			"Copyright Team Awesome, Program version: Awesome",
-			// title:
-			"Licensed under GPL v3",
-			MB_OK | MB_ICONEXCLAMATION
-		);
-		break;
-	case ID_FILE_EXIT:
-		PostQuitMessage(0);
-		break;
-	}
 
 	void Window::OnCommand(int from, int command) {
 		switch(from){
