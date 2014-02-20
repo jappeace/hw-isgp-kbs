4fd4d550d485736aa50417769358794c81ab12bc
Joep Kemperman <kazooiej@gmail.com>
2014-02-20 10:12:02 +0100

    Added LevelImporter class, partially implemented.

---
+++ ISGPKBS/LevelEditor/LevelImporter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelEditor
+{
+	class LevelImporter : ILevelImporter
+	{
+		/// <summary>
+		/// Loads a level file and returns an ILevel object.
+		/// May throw a FileNotFoundException if the file is not found.
+		/// </summary>
+		public ILevel ImportLevel(string file)
+		{
+			ILevel level;
+			var streamReader = new StreamReader(file);
+
+			return null;
+		}
+
+		private string GetStringValue(string line)
+		{
+			// line.Split() returns ["name", "=", "value"].
+			// We return the third index, which is the value we want.
+			return line.Split('=')[2];
+		}
+	}
+}
