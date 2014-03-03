513964f59c0258645de4919c80b0e389437518b9
Joep Kemperman <kazooiej@gmail.com>
2014-03-03 12:07:20 +0100

    Removed test code from leveleditor form.

--- ISGPKBS/LevelEditor/LevelEditor.cs
+++ ISGPKBS/LevelEditor/LevelEditor.cs
@@ -12,17 +12,5 @@ namespace LevelEditor
 		{
 			InitializeComponent();
 		}
-
-		protected override void OnPaint(PaintEventArgs e)
-		{
-			
-			levelPanel.CreateGraphics().FillEllipse(new SolidBrush(Color.Red), 100, 100, 10000, 10000);
-			base.OnPaint(e);
-		}
-
-		private void loadToolStripMenuItem_Click(object sender, System.EventArgs e)
-		{
-
-		}
 	}
 }
