54d43d2058cdac3de6656086638d2939e87f5f86
Johnnei <johnnei07@live.nl>
2014-03-03 12:10:14 +0100

    Corrected gravitational pull calculation.

--- ISGPKBS/Game/gravitybehaviour.cpp
+++ ISGPKBS/Game/gravitybehaviour.cpp
@@ -16,13 +16,13 @@ namespace isgp {
 		}
 
 		// Calculate downward motion
-		const double GRAVITATIONAL_PULL = (double) 2.45;
+		const double GRAVITATIONAL_PULL = (double) 9.81;
 		const double TIME_MULTIPLIER = ms / (double) 1000;
 
 		// The calculation needs to keep in mind that the applied gravity will get removed by 1 unit per second.
 		double gravity = TIME_MULTIPLIER * GRAVITATIONAL_PULL; // The amount of velocity which should be applied at this update
 
 		// Apply downward motion onto character
-		_player->AddToVelocityY(GRAVITATIONAL_PULL);
+		_player->AddToVelocityY(gravity);
 	}
 }
