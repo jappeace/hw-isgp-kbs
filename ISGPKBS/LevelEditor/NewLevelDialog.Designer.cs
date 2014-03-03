88db1102e155121a6a39e862dd61b194136f6976
Joep Kemperman <kazooiej@gmail.com>
2014-03-03 12:20:47 +0100

    Created simple dialog box for new level.

--- ISGPKBS/LevelEditor/NewLevelDialog.Designer.cs
+++ ISGPKBS/LevelEditor/NewLevelDialog.Designer.cs
@@ -28,11 +28,92 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.widthLabel = new System.Windows.Forms.Label();
+			this.heightLabel = new System.Windows.Forms.Label();
+			this.widthBox = new System.Windows.Forms.TextBox();
+			this.heightBox = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// widthLabel
+			// 
+			this.widthLabel.AutoSize = true;
+			this.widthLabel.Location = new System.Drawing.Point(13, 16);
+			this.widthLabel.Name = "widthLabel";
+			this.widthLabel.Size = new System.Drawing.Size(35, 13);
+			this.widthLabel.TabIndex = 0;
+			this.widthLabel.Text = "Width";
+			// 
+			// heightLabel
+			// 
+			this.heightLabel.AutoSize = true;
+			this.heightLabel.Location = new System.Drawing.Point(12, 38);
+			this.heightLabel.Name = "heightLabel";
+			this.heightLabel.Size = new System.Drawing.Size(38, 13);
+			this.heightLabel.TabIndex = 1;
+			this.heightLabel.Text = "Height";
+			// 
+			// widthBox
+			// 
+			this.widthBox.Location = new System.Drawing.Point(90, 12);
+			this.widthBox.Name = "widthBox";
+			this.widthBox.Size = new System.Drawing.Size(100, 20);
+			this.widthBox.TabIndex = 2;
+			// 
+			// heightBox
+			// 
+			this.heightBox.Location = new System.Drawing.Point(90, 35);
+			this.heightBox.Name = "heightBox";
+			this.heightBox.Size = new System.Drawing.Size(100, 20);
+			this.heightBox.TabIndex = 3;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(115, 67);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Create level";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button2.Location = new System.Drawing.Point(33, 67);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Cancel";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// NewLevelDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "NewLevelDialog";
+			this.ClientSize = new System.Drawing.Size(202, 97);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.heightBox);
+			this.Controls.Add(this.widthBox);
+			this.Controls.Add(this.heightLabel);
+			this.Controls.Add(this.widthLabel);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "NewLevelDialog";
+			this.Text = "New level";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label widthLabel;
+		private System.Windows.Forms.Label heightLabel;
+		private System.Windows.Forms.TextBox widthBox;
+		private System.Windows.Forms.TextBox heightBox;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
 	}
 }
