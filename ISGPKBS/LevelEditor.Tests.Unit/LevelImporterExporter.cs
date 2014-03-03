15d229ffb38bd2a6d19d6bca99d9056011016deb
Joep Kemperman <kazooiej@gmail.com>
2014-03-03 10:14:18 +0100

    Added unit test file.

---
+++ ISGPKBS/LevelEditor.Tests.Unit/LevelImporterExporter.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using LevelEditor.IO;
+using LevelEditor.Tests.Unit.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LevelEditor.Tests.Unit
+{
+	[TestClass]
+	public class LevelImporterExporter
+	{
+		[TestMethod]
+		public void Import_ExportedLevel_SameLevel()
+		{
+			ILevel level = new Level(10, 10);
+			level.SetTile(new Point(3, 2), TileType.Brick);
+			level.SetTile(new Point(5, 9), TileType.Start);
+			StringWriter stringWriter = new StringWriter();
+			ILevelExporter levelExporter = new LevelExporter(stringWriter);
+			levelExporter.ExportLevel(level);
+			StringReader stringReader = new StringReader(stringWriter.LevelString);
+			ILevelImporter levelImporter = new LevelImporter(stringReader);
+			ILevel importedLevel = levelImporter.ImportLevel();
+		}
+	}
+}
