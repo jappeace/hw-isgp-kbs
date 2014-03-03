436e0079eb00873feeda6b54bf70b0d33c61e277
Joep Kemperman <kazooiej@gmail.com>
2014-03-03 10:52:23 +0100

    Added levelpanel to the form.

--- ISGPKBS/LevelEditor/LevelEditor.Designer.cs
+++ ISGPKBS/LevelEditor/LevelEditor.Designer.cs
@@ -28,13 +28,26 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.levelPanel = new System.Windows.Forms.Panel();
 			this.SuspendLayout();
+			// 
+			// levelPanel
+			// 
+			this.levelPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.levelPanel.BackColor = System.Drawing.SystemColors.ButtonShadow;
+			this.levelPanel.Location = new System.Drawing.Point(12, 12);
+			this.levelPanel.Name = "levelPanel";
+			this.levelPanel.Size = new System.Drawing.Size(361, 261);
+			this.levelPanel.TabIndex = 0;
 			// 
 			// LevelEditor
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(688, 521);
+			this.Controls.Add(this.levelPanel);
 			this.Name = "LevelEditor";
 			this.Text = "Megadude++ Leveleditor";
 			this.ResumeLayout(false);
@@ -42,6 +55,8 @@
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel levelPanel;
 	}
 }
 
