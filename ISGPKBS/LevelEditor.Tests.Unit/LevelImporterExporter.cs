42673c2770069dd06ec6af630bf0cd9d0c3217d3
Joep Kemperman <kazooiej@gmail.com>
2014-03-03 10:27:14 +0100

    LevelImporterExporter unit test now ends the levelstring with \r\n, so the unit test doesnt fail when it shouldn't.

--- ISGPKBS/LevelEditor.Tests.Unit/LevelImporterExporter.cs
+++ ISGPKBS/LevelEditor.Tests.Unit/LevelImporterExporter.cs
@@ -12,7 +12,7 @@ namespace LevelEditor.Tests.Unit
 		{
 			// Create simple levelstring.
 			string levelString = "width=10\r\nheight=10\r\n";
-			levelString += "1,2=1\r\n5,3=0";
+			levelString += "1,2=1\r\n5,3=0\r\n";
 
 			// Import from levelstring to ILevel object.
 			ILevelImporter importer = new LevelImporter(new StringReader(levelString));
