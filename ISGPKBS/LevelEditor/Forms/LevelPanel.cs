697ed071c5ed9344b2e21404d506be154338bd96
Joep Kemperman <kazooiej@gmail.com>
2014-03-04 11:56:52 +0100

    Added LevelPanel.

---
+++ ISGPKBS/LevelEditor/Forms/LevelPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace LevelEditor.Forms
+{
+	class LevelPanel : Panel
+	{
+		public LevelPanel()
+		{
+			// Enable double buffering for a smoother user experience.
+			DoubleBuffered = true;
+			// Enable scrollbars.
+			AutoScroll = true;
+			BackColor = Color.AliceBlue;
+		}
+
+		/// <summary>
+		/// Draws the level on the panel.
+		/// </summary>
+		/// <param name="e"></param>
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+		}
+	}
+}
