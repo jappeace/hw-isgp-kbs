bd30f824b4fb4fb2380d264500ea97edfa43afcd
Johnnei <johnnei07@live.nl>
2014-03-10 15:01:14 +0100

    Added more comments to the sprite constructor to explain everything in more detail.

--- ISGPKBS/Game/Sprite.cpp
+++ ISGPKBS/Game/Sprite.cpp
@@ -9,24 +9,26 @@ Sprite::Sprite(HBITMAP bitmap)
 {
 	this->_image = bitmap;
 
-	// Create mask
+	// Create variables to work with the given image.
+	HDC imgHDC = CreateCompatibleDC(NULL);
 	BITMAP image;
 	GetObject(this->_image, sizeof(BITMAP), &image);
+	SelectObject(imgHDC, this->_image);
 
-	BITMAP mask = BITMAP();
-	HDC imgHDC = CreateCompatibleDC(NULL);
+	// Create variables to create the mask
+	BITMAP mask;
 	HDC tmpHDC = CreateCompatibleDC(NULL);
 	this->_mask = CreateCompatibleBitmap(tmpHDC, image.bmWidth, image.bmHeight);
 	GetObject(this->_mask, sizeof(BITMAP), &mask);
-
-	SelectObject(imgHDC, this->_image);
 	SelectObject(tmpHDC, this->_mask);
 
+	// Define some constants which contain the values for transparancy correction.
 	static const int kKeyColor       = 0xff00ff; // Transparancy key color
 	static const int kPreserveColor  = 0xffffff; // A color which honors the backbuffer data instead of the image
 	static const int kOverwriteColor = 0x000000; // A color which honors the image data instead of the backbuffer
 	static const int kBlack          = 0x000000; // The hex value of the color black
 
+	// Generate alpha-mask and overwrite image pixels if needed.
 	for (int y = 0; y < image.bmHeight; ++y) {
 		for (int x = 0; x < image.bmWidth; ++x) {
 			int color = GetPixel(imgHDC, x, y);
@@ -39,6 +41,7 @@ Sprite::Sprite(HBITMAP bitmap)
 		}
 	}
 
+	// Clean up no longer needed resources
 	DeleteDC(imgHDC);
 	DeleteDC(tmpHDC);
 }
