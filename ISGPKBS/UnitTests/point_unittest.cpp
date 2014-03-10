7711a2788e77d841dede93c4927c511ee1bc437c
Jappie Klooster <jappieklooster@hotmail.com>
2014-03-10 15:31:51 +0100

    added unit tests to explain vector

--- ISGPKBS/UnitTests/point_unittest.cpp
+++ ISGPKBS/UnitTests/point_unittest.cpp
@@ -1,8 +1,9 @@
 #include "stdafx.h"
-#include "CppUnitTest.h"
+#include <CppUnitTest.h>
 #include "../Game/Vector2D.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace isgp;
 
 namespace UnitTests
 {		
@@ -12,23 +13,46 @@ namespace UnitTests
 		
 		TEST_METHOD(Getter)
 		{
-			isgp::Vector2D p(5, 7);
+			Vector2D p(5, 7);
 			Assert::AreEqual(5.0, p.X());
 			Assert::AreEqual(7.0, p.Y());
 		}
 
 		TEST_METHOD(Setter) {
-			isgp::Vector2D p(5, 7);
+			Vector2D p(5, 7);
 			p.X(p.X() + 1);
 			p.Y(p.Y() - 2);
 			Assert::AreEqual(6.0, p.X());
 			Assert::AreEqual(5.0, p.Y());
 		}
+
 		TEST_METHOD(AddOverload) {
-			isgp::Vector2D p(5, 7);
-			p -= isgp::Vector2D(1, -2);
+			Vector2D p(5, 7);
+			p += Vector2D(1, -2);
 			Assert::AreEqual(6.0, p.X());
 			Assert::AreEqual(5.0, p.Y());
+		}
+		TEST_METHOD(RemoveOverload) {
+			Vector2D p(5, 7);
+			p -= Vector2D(1, -2);
+			Assert::AreEqual(4.0, p.X());
+			Assert::AreEqual(9.0, p.Y());
+		}
+		TEST_METHOD(DivideOverload) {
+			Vector2D p(5, 6);
+			p /= Vector2D(2, 3);
+			Assert::AreEqual(2.5, p.X());
+			Assert::AreEqual(2.0, p.Y());
+		}
+		TEST_METHOD(MulitplyOverload) {
+			Vector2D p(5, 6);
+			p *= Vector2D(2, 3);
+			Assert::AreEqual(10.0, p.X());
+			Assert::AreEqual(18.0, p.Y());
+
+			p = p * Vector2D(2, 3);
+			Assert::AreEqual(20.0, p.X());
+			Assert::AreEqual(54.0, p.Y());
 		}
 	};
 }
