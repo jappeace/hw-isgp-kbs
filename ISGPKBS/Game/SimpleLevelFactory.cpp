b4d834d73f1a845b9e585cb5cfce11649b41bc51
Joep Kemperman <kazooiej@gmail.com>
2014-03-12 11:34:59 +0100

    Made the level smaller to improve performance during debugging.

--- ISGPKBS/Game/SimpleLevelFactory.cpp
+++ ISGPKBS/Game/SimpleLevelFactory.cpp
@@ -12,16 +12,14 @@ namespace isgp{
 	}
 
 	Level* SimpleLevelFactory::CreateLevel(){
-		Level* result = new Level(new Grid(30,20));
+		Level* result = new Level(new Grid(30,10));
 		Grid * g = result->GetGrid();
-		
-		g->GetTileAt(Vector2D(10, 10))->SetData(new WorldBlock("C:\\tiles\\smile.bmp"));
-		g->GetTileAt(Vector2D(10, 11))->SetData(new WorldBlock("C:\\tiles\\smile.bmp"));
-		g->GetTileAt(Vector2D(10, 12))->SetData(new WorldBlock("C:\\tiles\\smile.bmp"));
 
-		g->TraverseRow(18, this);
+		g->TraverseRow(9, this);
+		g->TraverseCollumn(0, this);
+		g->TraverseCollumn(29, this);
 
-		result->_player = new Player(Vector2D(200,100));
+		result->_player = new Player(Vector2D(100,70));
 		result->_player->SetGrid(g);
 		result->_enemy = new Ghost(Vector2D(100,221), result->_player);
 		result->_enemy2 = new Patrol(Vector2D(100,221), 200, result->_player);
