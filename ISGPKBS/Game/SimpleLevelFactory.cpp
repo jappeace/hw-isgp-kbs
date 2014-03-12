376ef9c7a110dd22066425660ee11015afb24646
leander <leandernieuwenstein@hotmail.com>
2014-03-12 12:21:52 +0100

    wutwut

--- ISGPKBS/Game/SimpleLevelFactory.cpp
+++ ISGPKBS/Game/SimpleLevelFactory.cpp
@@ -24,10 +24,20 @@ namespace isgp{
 		
 		g->GetTileAt(Vector2D(41, 17))->SetData(new WorldBlock(".\\tiles\\smile.bmp"));
 		g->GetTileAt(Vector2D(42, 17))->SetData(new WorldBlock(".\\tiles\\smile.bmp"));
+		
+		g->GetTileAt(Vector2D(55, 13))->SetData(new WorldBlock(".\\tiles\\smile.bmp"));
+		g->GetTileAt(Vector2D(56, 13))->SetData(new WorldBlock(".\\tiles\\smile.bmp"));
+		g->GetTileAt(Vector2D(57, 13))->SetData(new WorldBlock(".\\tiles\\smile.bmp"));
+		g->GetTileAt(Vector2D(58, 13))->SetData(new WorldBlock(".\\tiles\\smile.bmp"));
+		g->GetTileAt(Vector2D(59, 13))->SetData(new WorldBlock(".\\tiles\\smile.bmp"));
+		g->GetTileAt(Vector2D(60, 13))->SetData(new WorldBlock(".\\tiles\\smile.bmp"));
+		g->GetTileAt(Vector2D(61, 13))->SetData(new WorldBlock(".\\tiles\\smile.bmp"));
+		g->GetTileAt(Vector2D(62, 13))->SetData(new WorldBlock(".\\tiles\\smile.bmp"));
+		g->GetTileAt(Vector2D(63, 13))->SetData(new WorldBlock(".\\tiles\\smile.bmp"));
 
 		g->TraverseRow(18, this);
 
-		result->_player = new Player(Vector2D(500,256));
+		result->_player = new Player(Vector2D(500,250));
 		result->_player->SetGrid(g);
 		result->_enemy = new Ghost(Vector2D(2000,256), result->_player);
 		result->_enemy2 = new Patrol(Vector2D(200,256), 200, result->_player);
