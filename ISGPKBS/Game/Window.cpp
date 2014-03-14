6f787939da09356cbfc94bc7c382e20abfa50165
leander <leandernieuwenstein@hotmail.com>
2014-03-14 12:02:11 +0100

    paralex scrolling is now done!

--- ISGPKBS/Game/Window.cpp
+++ ISGPKBS/Game/Window.cpp
@@ -45,25 +45,26 @@ INT_PTR CALLBACK dialogProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
 	return NULL;
 }
 void Window::OnPaint(Graphics* g){
-	if (_cam != NULL) {
-		double width1 = (GridGraphicTranslator().FromTo(*_level->GetGrid()->GetSize()).X() * 0.5) + AbstractWindow::WindowSize.GetWidth();
-		double width2 = (GridGraphicTranslator().FromTo(*_level->GetGrid()->GetSize()).X() * 0.75) + AbstractWindow::WindowSize.GetWidth();
+	if (_gameState == Playing) {
+		if (_cam != NULL) {
+			double width1 = (GridGraphicTranslator().FromTo(*_level->GetGrid()->GetSize()).X() * 0.5) + AbstractWindow::WindowSize.GetWidth();
+			double width2 = (GridGraphicTranslator().FromTo(*_level->GetGrid()->GetSize()).X() * 0.75) + AbstractWindow::WindowSize.GetWidth();
 
-		Vector2D fix = ((AbstractWindow::WindowSize - Player::InitSize) * Vector2D(1.0,1.35)) / Vector2D(2);
+			Vector2D fix = ((AbstractWindow::WindowSize - Player::InitSize) * Vector2D(1.0,1.35)) / Vector2D(2);
 
-		//Draw sky
-		for (int i = 0; i < width1; i += 1920) {
-			g->DrawBitmap("../tiles/mountain.bmp", Vector2D(((_cam->GetPosition().X() - fix.X()) * 0.5) + i, (_cam->GetPosition().Y() - fix.Y()) * 0.5), Size(1920, 791));
-		}
+			//Draw sky
+			for (int i = 0; i < width1; i += 1920) {
+				g->DrawBitmap("../tiles/mountain.bmp", Vector2D(((_cam->GetPosition().X() - fix.X()) * 0.5) + i, (_cam->GetPosition().Y() - fix.Y()) * 0.5), Size(1920, 791));
+			}
 	
-		//Draw ground
-		for (int i = 0; i < width2; i += 1920) {
-			g->DrawBitmap("../tiles/ground.bmp", Vector2D(((_cam->GetPosition().X() - fix.X()) * 0.25) + i, (_cam->GetPosition().Y() + 2000) * 0.25), Size(1920, 321));
-		}
+			//Draw ground
+			for (int i = 0; i < width2; i += 1920) {
+				g->DrawBitmap("../tiles/ground.bmp", Vector2D(((_cam->GetPosition().X() - fix.X()) * 0.25) + i, (_cam->GetPosition().Y() + 2000) * 0.25), Size(1920, 321));
+			}
 
-		//Draw everything else
-		_level->Paint(g);
-	}
+			//Draw everything else
+			_level->Paint(g);
+		}
 	}
 	else if (_gameState == GameOver) {
 		_currentMenu->Paint(g);
