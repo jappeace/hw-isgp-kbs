e76913ade64fa06da2624f961d1317243b6a847d
Johnnei <johnnei07@live.nl>
2014-03-14 11:03:52 +0100

    Collision now checks every half pixel.
    Removed fix to get floor collision.
    Collision now seems fine.

--- ISGPKBS/Game/Player.cpp
+++ ISGPKBS/Game/Player.cpp
@@ -116,7 +116,6 @@ const Size Player::InitSize(32, 32);
 			g->DrawRect(Vector2D((int)p->X(), (int)p->Y()), Vector2D((int)p->X() + 16, (int)p->Y() + 16));
 		}
 		g->SetColor(RGB(0, 0, 0));
-
 		g->DrawStr(Vector2D(10, 40), collision & Up ? "Up: true" : "Up: false");
 		g->DrawStr(Vector2D(10, 55), collision & Down ? "Down: true" : "Down: false");
 		g->DrawStr(Vector2D(10, 70), collision & Left ? "Left: true" : "Left: false");
