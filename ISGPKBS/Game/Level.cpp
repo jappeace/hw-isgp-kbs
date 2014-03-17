1f4e2024f2daea5becc59620d1f501ecc3fec21e
Joep Kemperman <kazooiej@gmail.com>
2014-03-17 13:53:00 +0100

    Moved parralax scrolling to Level.cpp

--- ISGPKBS/Game/Level.cpp
+++ ISGPKBS/Game/Level.cpp
@@ -30,11 +30,27 @@ const Size	Level::tileSize = Size(TILE_WIDTH, TILE_HEIGHT);
 	}
 
 	void Level::Paint(Graphics* g) {
+		double width1 = (GridGraphicTranslator().FromTo(GetGrid()->GetSize()).X() * 0.5);// + AbstractWindow::WindowSize.GetWidth();
+		double width2 = (GridGraphicTranslator().FromTo(GetGrid()->GetSize()).X() * 0.75);// + AbstractWindow::WindowSize.GetWidth();
+
+		Vector2D fix = ((/*AbstractWindow::WindowSize - */Player::InitSize) * Vector2D(1.0,1.35)) / Vector2D(2);
+
+		//Draw sky
+		for (int i = 0; i < width1; i += 1920) {
+			g->DrawBitmap("../tiles/mountain.bmp", Vector2D(((_cam->GetPosition().X() - fix.X()) * 0.5) + i, (_cam->GetPosition().Y() - fix.Y()) * 0.5), Size(1920, 791));
+		}
+
+		//Draw ground
+		for (int i = 0; i < width2; i += 1920) {
+			g->DrawBitmap("../tiles/ground.bmp", Vector2D(((_cam->GetPosition().X() - fix.X()) * 0.25) + i, (_cam->GetPosition().Y() + 2000) * 0.25), Size(1920, 321));
+		}
+
 		_graphics = g;
 		_grid->TraverseTiles(this);
 		_player->Paint(g);
 		_enemy->Paint(g);
 		_enemy2->Paint(g);
+
 	}
 	void Level::ReceiveTile(Tile* tile) {
 #ifdef _DEBUG
--- ISGPKBS/Game/Window.cpp
+++ ISGPKBS/Game/Window.cpp
@@ -45,30 +45,6 @@ void Window::OnPaint(Graphics* g){
 	if (_gameState != NULL) {
 		_gameState->Paint(g);
 	}
-	//if (_gameState == NULL) {
-	//	if (_cam != NULL) {
-	//		double width1 = (GridGraphicTranslator().FromTo(*_level->GetGrid()->GetSize()).X() * 0.5) + AbstractWindow::WindowSize.GetWidth();
-	//		double width2 = (GridGraphicTranslator().FromTo(*_level->GetGrid()->GetSize()).X() * 0.75) + AbstractWindow::WindowSize.GetWidth();
-
-	//		Vector2D fix = ((AbstractWindow::WindowSize - Player::InitSize) * Vector2D(1.0,1.35)) / Vector2D(2);
-
-	//		//Draw sky
-	//		for (int i = 0; i < width1; i += 1920) {
-	//			//g->DrawBitmap("../tiles/mountain.bmp", Vector2D(((_cam->GetPosition().X() - fix.X()) * 0.5) + i, (_cam->GetPosition().Y() - fix.Y()) * 0.5), Size(1920, 791));
-	//		}
-	//
-	//		//Draw ground
-	//		for (int i = 0; i < width2; i += 1920) {
-	//			//g->DrawBitmap("../tiles/ground.bmp", Vector2D(((_cam->GetPosition().X() - fix.X()) * 0.25) + i, (_cam->GetPosition().Y() + 2000) * 0.25), Size(1920, 321));
-	//		}
-
-	//		//Draw everything else
-	//		_level->Paint(g);
-	//	}
-	//}
-	//else if (_gameState == NULL) {
-	//	_currentMenu->Paint(g);
-	//}
 }
 void Window::GameLoop(double elapsed) { //elapsed time, in MS
 	if (_gameState != NULL) {
