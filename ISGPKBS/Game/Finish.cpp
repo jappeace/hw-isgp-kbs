9ee51fe5da5937c3f269137295e0cbe6aa28792c
chris.m@Live.nl <chris.m@live.nl>
2014-03-18 16:03:00 +0100

    Added toad tracing the player.

--- ISGPKBS/Game/Finish.cpp
+++ ISGPKBS/Game/Finish.cpp
@@ -2,16 +2,25 @@
 
 namespace isgp {
 
-	Finish::Finish(Vector2D position)
+	Finish::Finish(Vector2D position, Player* player)
 	{
+		_player = player;
+		_animationOffset = Vector2D();
 		_position = GridGraphicTranslator().FromTo(position);
 		_animation = new Animation("../tiles/toad.bmp", Size(32, 32), 4, 150);
 	}
 	void Finish::Update(double elapsed) {
 		_animation->OnUpdate(elapsed);
+		if(_position.X() > _player->GetPosition().X() && !_lastWasLeft) {
+			_lastWasLeft = true;
+			_animationOffset = Vector2D();
+		} else if(_position.X() < _player->GetPosition().X() && _lastWasLeft) {
+			_lastWasLeft = false;
+			_animationOffset = Vector2D(96, 0);
+		}
 	}
 	void Finish::Paint(Graphics* graphics) {
-		_animation->Render(graphics, _position);
+		_animation->Render(graphics, _position, _animationOffset);
 	}
 
 	Vector2D Finish::GetPosition() {
--- ISGPKBS/Game/Finish.h
+++ ISGPKBS/Game/Finish.h
@@ -1,17 +1,21 @@
 #pragma once
 #include "Entity.h"
 #include "Animation.h"
+#include "Player.h"
 namespace isgp {
 	class Finish :
 		public Entity
 	{
 	public:
-		Finish(Vector2D);
+		Finish(Vector2D, Player*);
 		~Finish(void);
 		void Paint(Graphics*) override;
 		void Update(const double) override;
 		Vector2D GetPosition();
 	private:
+		Vector2D _animationOffset;
+		bool _lastWasLeft;
+		Player* _player;
 		Vector2D offset;
 		Animation* _animation;
 	};
