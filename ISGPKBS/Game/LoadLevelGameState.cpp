1f2e71d79ec34398f65bf21a2e10034b2475b21f
Joep Kemperman <kazooiej@gmail.com>
2014-03-21 10:42:47 +0100

    Improved code formatting for LoadLevelGameState classes.

--- ISGPKBS/Game/LoadLevelGameState.cpp
+++ ISGPKBS/Game/LoadLevelGameState.cpp
@@ -1,19 +1,20 @@
 #include "LoadLevelGameState.h"
 #include "Window.h"
 
-namespace isgp{
-	LoadLevelGameState::LoadLevelGameState(Window* parent, int levelnr, SpriteCache<int>* cache, void(Window::*startLevelCalback)(Level*, Camera*)) {
+namespace isgp {
+	LoadLevelGameState::LoadLevelGameState(Window* parent, int levelnr,
+			SpriteCache<int>* cache, void(Window::*startLevelCalback)(Level*, Camera*)) {
 		_window = parent;
 		_cache = cache;
 		_levelnr = levelnr;
 		_hasDrawn = false;
 		_startLevelCalback = startLevelCalback;
+		_theme = Theme::CreateTheme(_levelnr);
 	}
 
-	LoadLevelGameState::~LoadLevelGameState(void) {
-	}
+	LoadLevelGameState::~LoadLevelGameState(void) { }
+
 	void LoadLevelGameState::Paint(Graphics* graphics) {
-
 		if(!_hasDrawn){
 			RenderSplashScreen(graphics);
 			return;
@@ -21,35 +22,36 @@ namespace isgp{
 		pair<Level*, Camera*> loaded = LoadLevel(graphics);
 		(_window->*_startLevelCalback)(loaded.first, loaded.second);
 	}
-	void LoadLevelGameState::KeyDown(int keyCode) { /** ignore keys */}
-	void LoadLevelGameState::KeyUp(int keyCode) { /** ignore */}
-	void LoadLevelGameState::Update(double elapsed) { /** ignore */}
 
-	void LoadLevelGameState::RenderSplashScreen(Graphics* graphics){
+	void LoadLevelGameState::KeyDown(int keyCode) { /* ignore keys */ }
+	void LoadLevelGameState::KeyUp(int keyCode) { /* ignore */ }
+	void LoadLevelGameState::Update(double elapsed) { /* ignore */ }
 
-		// disable the anoying camera
+	void LoadLevelGameState::RenderSplashScreen(Graphics* graphics) {
+		// Disable the anoying camera
 		graphics->DeleteTranslater();
 		_hasDrawn = true;
-		// render splash screen
-		graphics->DrawStr(AbstractWindow::WindowSize / Vector2D(2.5,2.2), "Loading level");
-		graphics->DrawStr(AbstractWindow::WindowSize / Vector2D(3,2), "Don't forget, this game is awesome");
+		// Render splash screen
+		graphics->DrawStr(AbstractWindow::WindowSize / Vector2D(2.5,2.2),
+			"Loading level");
+		graphics->DrawStr(AbstractWindow::WindowSize / Vector2D(3,2),
+			"Don't forget, this game is awesome");
 	}
-	pair<Level*, Camera*> LoadLevelGameState::LoadLevel(Graphics* graphics){
-		// to store results in
+
+	pair<Level*, Camera*> LoadLevelGameState::LoadLevel(Graphics* graphics) {
+		// To store results in
 		pair<Level*, Camera*> result;
 
 		graphics->SetTranslator(_window->_cam);
-		Theme* theme = Theme::CreateTheme(_levelnr);
-		theme->LoadContent(graphics);
-		result.first = DefaultlevelFactory().CreateLevel(_levelnr, theme);
+		result.first = DefaultlevelFactory().CreateLevel(_levelnr, _theme);
 		BackgroundArtist artist = BackgroundArtist(_window->_cam, result.first, /* out */_cache);
-		artist.RenderBackground(); // wil handle caching by itself
+		artist.RenderBackground(); // Will handle caching by itself
 
 		graphics->SetTextBackgroundColor(RGB(255, 255, 255));
 		result.second = new Camera(result.first->_player, result.first->GetGrid());
 		graphics->SetTranslator(result.second);
 
-		// returns a copy of the pointer pair
+		// Returns a copy of the pointer pair
 		return result;
 	}
 }
